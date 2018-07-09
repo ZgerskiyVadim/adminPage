3e0c673511748f6dad3ad8fabeca837f1c427faf
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-07-09 13:05:10 +0300

    commit

--- server/models/group/group.js
+++ server/models/group/group.js
@@ -4,7 +4,7 @@ import User from '../user/user';
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-    name: {type: String, required: true, unique: true, minlength: 4, maxlength: 20},
+    name: {type: String, required: true, unique: true, minlength: 4, maxlength: 20, match: /[A-Za-z0-9\.\,\-]+/g},
     title: {type: String, required: true, minlength: 6, maxlength: 20},
     users: [{type: Schema.Types.ObjectId, ref: User}]
 });
--- server/models/user/user.js
+++ server/models/user/user.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, required: true, unique: true, minlength: 4, maxlength: 20},
-    firstName: {type: String, required: true, minlength: 2, maxlength: 20},
-    lastName: {type: String, required: true, minlength: 2, maxlength: 20},
+    username: {type: String, required: true, unique: true, minlength: 4, maxlength: 20, match: /[A-Za-z0-9\.\,\-]+/g},
+    firstName: {type: String, required: true, minlength: 2, maxlength: 20, match: /^[a-zA-Z\s\-]*$/},
+    lastName: {type: String, required: true, minlength: 2, maxlength: 20, match: /^[a-zA-Z]*$/},
     email: {type: String, required: true, unique: true, minlength: 3, maxlength: 30, match: /\S+@\S+\.\S+/},
 });
 
