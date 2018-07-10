3770318e7ee9ec66dc3444d343808f20892918b1
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-07-10 13:05:14 +0300

    commit

--- server/libs/logger/index.js
+++ server/libs/logger/index.js
@@ -2,6 +2,12 @@ import winston from 'winston';
 import config from '../../../config';
 
 const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.simple()
+    ),
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({ filename: config.loggerFileName })
