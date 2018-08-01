ac89f2376a21710aac0266aefded497c712004f2
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-08-01 12:02:33 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ import routers from './routers';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, config.favicon)));
 app.use('/api', routers);
