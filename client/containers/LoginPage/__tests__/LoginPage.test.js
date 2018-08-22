de74036aac412a81f1bcdc8c38d0cbddc30b6234
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-08-22 14:09:44 +0300

    commit

--- client/containers/LoginPage/__tests__/LoginPage.test.js
+++ client/containers/LoginPage/__tests__/LoginPage.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import {LoginPage} from '../index';
+import * as actions from '../../../actions/action_creators/users';
+import {usernameEvent, passwordEvent} from "./data";
 
 describe('LoginPage component', () => {
 
@@ -19,5 +21,65 @@ describe('LoginPage component', () => {
         expect(component.find('.form-control').at(1).prop('name')).toBe('password');
 
     });
+
+    it('should call "handleChange" and set state after change username form', () => {
+        const component = shallow(<LoginPage />);
+
+        component.find('[name="username"]').at(0).simulate('change', usernameEvent);
+        expect(component.state().username).toBe(usernameEvent.target.value);
+
+    });
+
+    it('should call "handleChange" and set state after change password form', () => {
+        const component = shallow(<LoginPage />);
+
+        component.find('[name="password"]').at(0).simulate('change', passwordEvent);
+        expect(component.state().password).toBe(passwordEvent.target.value);
+    });
+
+    it('call "login" after click button "send" in LoginPage component', () => {
+        const mockLoginUser = jest.fn();
+        const component = shallow(<LoginPage
+            actions={{...actions, userLoginRequest: mockLoginUser}}
+        />);
+        const expectedLoginUser = {
+            username: usernameEvent.target.value,
+            password: passwordEvent.target.value
+        };
+        const event = {
+            preventDefault: jest.fn()
+        };
+
+        component.find('[name="username"]').at(0).simulate('change', usernameEvent);
+        component.find('[name="password"]').at(0).simulate('change', passwordEvent);
+
+        component.find('.login__send').simulate('click', event);
+
+        const [call = []] = mockLoginUser.mock.calls;
+        expect(call).toEqual([expectedLoginUser]);
+    });
+
+    it('call "handle error" after call this func in LoginPage component', () => {
+        const component = shallow(<LoginPage />);
+        let error = {
+            message: 'Some error'
+        };
+        component.instance().handleError(error);
+
+        error = {
+            response: {
+                status: 401
+            }
+        };
+        component.instance().handleError(error);
+
+        error = {
+            response: {
+                status: 400
+            }
+        };
+
+        component.instance().handleError(error);
+    })
 
 });
---
+++ client/containers/LoginPage/__tests__/data.js
@@ -0,0 +1,13 @@
+export const usernameEvent = {
+    target: {
+        name: 'username',
+        value: 'username value'
+    }
+};
+
+export const passwordEvent = {
+    target: {
+        name: 'password',
+        value: 'password value'
+    }
+};
