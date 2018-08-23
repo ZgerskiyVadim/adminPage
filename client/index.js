687ee07a80d7c667892040697e504ea31ebd73d6
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-08-23 12:07:05 +0300

    commit

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {Router} from 'react-router-dom';
+import './styles.scss';
 
 import {configureStore} from './store/configureStore';
 import Header from './components/Header';
