173f7b6bfc750da7c011526b684d9d39bf2f0fcf
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-09-03 17:04:11 +0300

    commit

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import LoggedRedirectOnUsersPage from './components/LoggedRedirectOnUsersPage';
-import Groups from './containers/GroupsTablePage';
+import GroupsTablePage from './containers/GroupsTablePage';
 import GroupPage from './containers/GroupPage';
-import Users from './containers/UsersTablePage';
+import UsersTablePage from './containers/UsersTablePage';
 import UserPage from './containers/UserPage';
 import CreatePage from './containers/CreatePage';
 import LoginPage from './containers/LoginPage';
@@ -19,9 +19,9 @@ export default class Routes extends Component {
             <Switch>
                 <LoggedRedirectOnUsersPage exact path="/" component={LoginPage}/>
                 <Route exact path="/create" component={CreatePage}/>
-                <PrivateRoute exact path="/users" component={Users}/>
+                <PrivateRoute exact path="/users" component={UsersTablePage}/>
                 <PrivateRoute path="/users/:id" component={UserPage}/>
-                <PrivateRoute exact path="/groups" component={Groups}/>
+                <PrivateRoute exact path="/groups" component={GroupsTablePage}/>
                 <PrivateRoute path="/groups/:id" component={GroupPage}/>
                 <PrivateRoute exact path="/create-user" component={CreateUser}/>
                 <PrivateRoute exact path="/create-group" component={CreateGroup}/>
